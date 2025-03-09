a5336f413da4f9f7f2bdc3c107a5058224b03d32
LOLITEMAULTES <188165912+lolitemaultes@users.noreply.github.com>
2025-03-09 11:00:19 +1100

    Update server.js

--- web-demo/server.js
+++ web-demo/server.js
@@ -38,7 +38,7 @@ app.get('/flood-data', async (req, res) => {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml',
             'Accept-Language': 'en-US,en;q=0.9',
-            'Referer': 'https://www.bom.gov.au/',
+            'Referer': 'http://www.bom.gov.au/',
             'Cache-Control': 'no-cache'
           },
           timeout: 10000,
